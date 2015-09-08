46cb15a5dc9aa613b21c4b800b948ffa9bc03a68
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2015-09-07 21:23:59 -0300

    testing prepare stage

--- tests/test_base.py
+++ tests/test_base.py
@@ -2,13 +2,13 @@
 
 import os
 
-from mock import patch, Mock
+from mock import patch, Mock, MagicMock
 import unittest
 
 import mclevel
+import box
 
-from box import BoundingBox
-
+import customine.base
 from customine.base import BaseBuilder
 
 
@@ -44,6 +44,53 @@ class TestBaseBuilder(unittest.TestCase):
 
         self.assertEquals(level, 'some_level')
         mclevel.fromFile.assert_called_once_with(level_path)
+
+
+    # def prepare_stage(self):
+    #     """
+    #     ensure chunks in given size, considering init_pos, do exist
+    #     and their chunk positions (also set as `self.chunk_positions`)
+    #     """
+    #     if self.init_pos is None or self.level is None:
+    #         return
+
+    #     x, y, z = self.init_pos
+
+    #     bbox = BoundingBox(origin=(x, y, z), size=self.size)
+    #     chunk_positions = bbox.chunkPositions
+    #     self.level.createChunksInBox(bbox)
+    #     self.chunk_positions = chunk_positions
+    #     return chunk_positions
+
+    def test_prepare_stage_should_return_none_if_no_init_pos_or_level_is_set(self):
+        init_pos = [-269, 4, 1584]
+        b = BaseBuilder(level_path=None, init_pos=init_pos)
+
+        ret = b.prepare_stage()
+        self.assertEquals(ret, None)
+
+        b = BaseBuilder(level_path=None, init_pos=None)
+        b.level = "loaded_level"
+
+        ret = b.prepare_stage()
+        self.assertEquals(ret, None)
+
+    @patch('customine.base.BoundingBox', Mock())
+    def test_prepare_stage_should_instantiate_box_with_correct_sizes_and_origin(self):
+        init_pos = [1, 2, 3]
+        size = [6, 5, 4]
+        bb = BaseBuilder(level_path=None, init_pos=init_pos, size=size)
+
+        #mocks
+        customine.base.BoundingBox.return_value.chunkPositions = 'chunk_positions'
+        bb.level = Mock()
+        bb.level.createChunksInBox = Mock()
+
+        ret_chunks = bb.prepare_stage()
+        self.assertEquals(ret_chunks, 'chunk_positions')
+        customine.base.BoundingBox.assert_called_once_with(
+            origin=tuple(init_pos), size=size
+        )
 
     def tearDown(self):
         # ensure the file locks are closed
